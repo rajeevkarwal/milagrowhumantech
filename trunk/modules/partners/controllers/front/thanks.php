5ac99bf85dd60f87f155b08ecdc416ca7a6202dd
Milagrow <gunjan.srivastava@milagrow.in>
2016-06-28 17:46:30 +0530

    updated pages

--- trunk/modules/partners/controllers/front/thanks.php
+++ trunk/modules/partners/controllers/front/thanks.php
@@ -7,6 +7,7 @@ class PartnersThanksModuleFrontController extends ModuleFrontController
 		parent::initContent();
 		$this->setTemplate('Thanks.tpl');
 		$server_name=$_SERVER['SERVER_NAME'];
+		//redirecting page back to home
 		header('Refresh: 10;url:'.$server_name.'/partners-with-us');
 	}
 }
